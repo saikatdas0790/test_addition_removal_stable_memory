0b4a3c221f6f61dc1ea89ad6a6a498d868182670
Saikat Das <saikatdas0790@gmail.com>
2023-02-17 20:10:55 +0530

    chore: upgrade using 2 versions and Candid

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -9,31 +9,41 @@ use ic_cdk::{
 #[derive(Default, CandidType, Deserialize)]
 pub struct CanisterData {
     counter_1: u64,
-    // #[serde(default)]
-    // counter_2: u64,
+}
+
+#[derive(Default, CandidType, Deserialize)]
+pub struct CanisterDataV2 {
+    counter_1: u128,
 }
 
 thread_local! {
-    static CANISTER_DATA: RefCell<CanisterData> = RefCell::default();
+    // static CANISTER_DATA: RefCell<CanisterData> = RefCell::default();
+    // static CANISTER_DATA_V2: RefCell<CanisterDataV2> = RefCell::default();
+    static CANISTER_DATA_V2: RefCell<CanisterDataV2> = RefCell::default();
 }
 
-#[ic_cdk::query]
-#[candid::candid_method(query)]
-fn get_counter_1() -> u64 {
-    CANISTER_DATA.with(|canister_data_ref_cell| canister_data_ref_cell.borrow().counter_1)
-}
+// #[ic_cdk::query]
+// #[candid::candid_method(query)]
+// fn get_counter_1() -> u64 {
+//     CANISTER_DATA.with(|canister_data_ref_cell| canister_data_ref_cell.borrow().counter_1)
+// }
 
-#[ic_cdk::update]
-#[candid::candid_method(update)]
-fn increment_counter_1() {
-    CANISTER_DATA.with(|canister_data_ref_cell| {
-        canister_data_ref_cell.borrow_mut().counter_1 += 1;
-    });
-}
+// #[ic_cdk::update]
+// #[candid::candid_method(update)]
+// fn increment_counter_1() {
+//     CANISTER_DATA.with(|canister_data_ref_cell| {
+//         canister_data_ref_cell.borrow_mut().counter_1 += 1;
+//     });
+// }
 
 #[ic_cdk::pre_upgrade]
 fn pre_upgrade() {
-    CANISTER_DATA.with(|canister_data_ref_cell| {
+    // CANISTER_DATA.with(|canister_data_ref_cell| {
+    //     let canister_data = canister_data_ref_cell.take();
+
+    //     storage::stable_save((canister_data,)).ok();
+    // });
+    CANISTER_DATA_V2.with(|canister_data_ref_cell| {
         let canister_data = canister_data_ref_cell.take();
 
         storage::stable_save((canister_data,)).ok();
@@ -42,16 +52,34 @@ fn pre_upgrade() {
 
 #[ic_cdk::post_upgrade]
 fn post_upgrade() {
-    match storage::stable_restore() {
+    // match storage::stable_restore() {
+    //     Ok((canister_data,)) => {
+    //         CANISTER_DATA.with(|canister_data_ref_cell| {
+    //             *canister_data_ref_cell.borrow_mut() = canister_data;
+    //         });
+    //     }
+    //     Err(e) => {
+    //         ic_cdk::print(format!("Error: {:?}", e));
+    //         panic!("Failed to restore canister data from stable memory");
+    //     }
+    // }
+    match storage::stable_restore::<(CanisterDataV2,)>() {
         Ok((canister_data,)) => {
-            CANISTER_DATA.with(|canister_data_ref_cell| {
+            CANISTER_DATA_V2.with(|canister_data_ref_cell| {
                 *canister_data_ref_cell.borrow_mut() = canister_data;
             });
         }
-        Err(e) => {
-            ic_cdk::print(format!("Error: {:?}", e));
-            panic!("Failed to restore canister data from stable memory");
-        }
+        Err(_) => match storage::stable_restore::<(CanisterData,)>() {
+            Ok((canister_data,)) => {
+                let canister_data_v2 = CanisterDataV2 {
+                    counter_1: canister_data.counter_1 as u128,
+                };
+                CANISTER_DATA_V2.with(|canister_data_ref_cell| {
+                    *canister_data_ref_cell.borrow_mut() = canister_data_v2;
+                });
+            }
+            Err(_) => panic!("Failed to decode with both V1 and V2"),
+        },
     }
 }
 
